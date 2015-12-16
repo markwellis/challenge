69f81124cc6271aced074126ec482bf813721949
Mark Ellis <m@rkellis.com>
2015-12-16 19:09:56 +0000

    created first draft of db schema

---
+++ sql/create.sql
@@ -0,0 +1,49 @@
+BEGIN;
+
+    --even though the spec seems to always use the same graph, it does
+    -- say each graph has an id, so make the db work that way, and it's
+    -- no extra work to make it support multiple, and makes it more useful
+
+    --why varchar(128)? there has to be a limit, and the spec doesn't say,
+    -- so it's as good as anything
+
+    --i'd normally give every table a serial id as the PK, but i've decided
+    -- it doesn't add any value here, and will make the code more complex,
+    -- as lookups are always clearest when done with the PK, which will be
+    -- the provided id. 
+
+    --nothing is mentioned about names being unique or used for lookups,
+    -- so i'm going to assume they're descriptive only
+
+    CREATE TABLE graphs (
+        "id"      varchar(128) NOT NULL,  --user provided id
+        "name"    varchar(128) NOT NULL,  --user provided name
+        PRIMARY KEY ("id")
+    );
+
+    CREATE TABLE nodes (
+        "id"          varchar(128) NOT NULL,  --user provided id
+        "graph_id"    varchar(128) NOT NULL,  --points to graphs.id
+        "name"        varchar(128) NOT NULL,  --user provided name
+        PRIMARY KEY ("id", "graph_id")        --we need the graph_id to lookup the node, and auto unique
+    );
+    ALTER TABLE "nodes" ADD CONSTRAINT "nodes-graph" FOREIGN KEY ("graph_id")
+        REFERENCES "graphs" ("id");
+
+    CREATE TABLE edges (
+        "id"          varchar(128) NOT NULL,                 --user provided id
+        "graph_id"    varchar(128) NOT NULL,                 --points to a graphs.id
+        "to"          varchar(128) NOT NULL,                 --this point's to nodes.id
+        "from"        varchar(128) NOT NULL,                 --this point's to nodes.id
+        "cost"        float CHECK ( "cost" >= 0 ) DEFAULT 0, --optional cost >= 0
+        PRIMARY KEY ("id", "graph_id"),
+        CONSTRAINT "edges-graph_id-to-from:unique" UNIQUE ("graph_id", "to", "from")
+    );
+    ALTER TABLE "edges" ADD CONSTRAINT "edges-graph" FOREIGN KEY ("graph_id")
+        REFERENCES "graphs" ("id");
+    ALTER TABLE "edges" ADD CONSTRAINT "edges-graph-to" FOREIGN KEY ("graph_id", "to")
+        REFERENCES "nodes" ("graph_id", "id");
+    ALTER TABLE "edges" ADD CONSTRAINT "edges-graph-from" FOREIGN KEY ("graph_id", "from")
+        REFERENCES "nodes" ("graph_id", "id");
+
+COMMIT;
--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,15 +1,32 @@
+#i should check if update has ran already today, but that's more work and update is quick enough
 sudo apt-get update
-sudo apt-get install -y postgresql-server-dev-9.4 build-essential perlbrew libexpat1-dev
 
-perlbrew init
-echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~/.bash_profile
-. ~/.bash_profile
+sudo apt-get install -y postgresql-9.4 postgresql-server-dev-9.4 build-essential perlbrew libexpat1-dev
 
 #why perlbrew? because the system perl is usually messed with, and this is cleaner and newer
-perlbrew install perl-5.22.1 -n
-perlbrew switch perl-5.22.1
-perlbrew install-cpanm
-cpanm Carton -n
+#setup perl
+if [ ! -f ~/perl5/perlbrew/etc/bashrc ]; then
+    perlbrew init
+    echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~/.bash_profile
+    . ~/.bash_profile
+fi
 
+if [[ ! $(perlbrew list | fgrep 'perl-5.22.1') ]]; then 
+    perlbrew install perl-5.22.1 -n
+    perlbrew switch perl-5.22.1
+    perlbrew install-cpanm
+    cpanm Carton -n
+fi
+
+#install the required dependencies
 cd /vagrant
 carton install
+
+#setup the db
+if [[ ! $(sudo -u postgres psql -t -c "SELECT datname FROM pg_database WHERE datname = 'challenge'") ]]; then 
+    sudo -u postgres psql -c "CREATE USER challenger WITH PASSWORD 'reallysecure';"
+    sudo -u postgres createdb challenge -O challenger
+
+    #do this as the challenger user so that the ownership is correct, and to test the user is setup correct
+    PGPASSWORD='reallysecure' psql -U challenger -h localhost challenge < /vagrant/sql/create.sql
+fi
