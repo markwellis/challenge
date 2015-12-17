ac4ea2a87d2695d3dce3311588fd431df56c2b17
Mark Ellis <m@rkellis.com>
2015-12-17 00:34:54 +0000

    commented the FK's in the sql create. although maybe not so well!

--- sql/create.sql
+++ sql/create.sql
@@ -27,6 +27,8 @@ BEGIN;
         "name"        varchar(128) NOT NULL,  --user provided name
         PRIMARY KEY ("id", "graph_id")        --we need the graph_id to lookup the node, and auto unique
     );
+
+    --this is so we can find the nodes of a graph
     ALTER TABLE "nodes" ADD CONSTRAINT "nodes-graph" FOREIGN KEY ("graph_id")
         REFERENCES "graphs" ("id");
 
@@ -39,10 +41,15 @@ BEGIN;
         PRIMARY KEY ("id", "graph_id"),
         CONSTRAINT "edges-graph_id-to-from:unique" UNIQUE ("graph_id", "to", "from")
     );
+    --this is so we can find the edges of a graph
     ALTER TABLE "edges" ADD CONSTRAINT "edges-graph" FOREIGN KEY ("graph_id")
         REFERENCES "graphs" ("id");
+
+    --this is for looking up the "to" part of the edge, so we can build the result
     ALTER TABLE "edges" ADD CONSTRAINT "edges-graph-to" FOREIGN KEY ("graph_id", "to")
         REFERENCES "nodes" ("graph_id", "id");
+
+    --this is for looking up the "from" part of the edge, so we can build the result
     ALTER TABLE "edges" ADD CONSTRAINT "edges-graph-from" FOREIGN KEY ("graph_id", "from")
         REFERENCES "nodes" ("graph_id", "id");
 
